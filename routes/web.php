3c074ed3ab890131ee7fc1eb87ede0ab9fdd1297
Martha Nieto <m27842n@outlook.com>
2022-12-29 20:27:20 -0800

    - wip: main - working on routes and views.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/blog', function () {
+    return 'Published blog list';
+});
+
+Route::get('/blog/{slug}', function ($slug) {
+    return 'Published blog with slug: ' . $slug;
 });
