ed8c55879cdfc5deea8b9b072c9fabcba7a75d04
Martha Nieto <m27842n@outlook.com>
2023-01-10 21:35:35 -0800

    - update: main - added template design.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     
-<h1>Home Cat Blog</h1>
+<h1 class="text-xl">Home Cat Blog</h1>
     
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <div class="container px-4 mx-auto">
-        <header class="flex justify-between items-center py-4">
+        <header class="flex justify-between items-center py-8">
             <div class="flex items-center flex-grow gap-4">
                 <a href="{{ route('home') }}">
                     <img src="{{ asset('images/logo.png') }}" alt="logo" class="h-12">
@@ -18,15 +18,31 @@
                     <input type="text" placeholder="Search">
                 </form>
             </div>
-
-            @auth
-            <a href="{{ route('dashboard') }}">Dashboard</a>   
-            @else
-            <a href="{{ route('login') }}">Login</a>
-            @endauth
+            <div class="text-xl">
+                @auth
+                <a href="{{ route('dashboard') }}">Dashboard</a>   
+                @else
+                <a href="{{ route('login') }}">Login</a>
+                @endauth
+            </div>
+            
         </header>
+        <div class="opacity-60 h-px mb-4" style="
+            background: linear-gradient(to right,
+            rgba(200, 200, 200, 0) 0%,
+            rgba(200, 200, 200, 1) 30%,
+            rgba(200, 200, 200, 1) 70%,
+            rgba(200, 200, 200, 0) 100%
+            );
+        ">
+
+        </div>
 
         @yield('content')
+
+        <p class="py-16">
+            <img src="{{ asset('images/logo.png') }}" alt="logo" class="h-12 mx-auto">
+        </p>
     </div>
 </body>
 </html>
